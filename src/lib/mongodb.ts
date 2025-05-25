89f4dd952114c9cea73b0d847f0377064a469bab
Jc007zZ <joaocpmcs@gmail.com>
2025-05-25 19:27:39 -0300

    modifed production

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -7,36 +7,21 @@ if (!process.env.MONGODB_URI) {
 const uri = process.env.MONGODB_URI;
 const options = {};
 
-let client;
+let client: MongoClient;
 let clientPromise: Promise<MongoClient>;
 
-// Definição do tipo global para persistência durante HMR
 declare global {
+  // Variável global para persistir conexão em dev e prod
   var _mongoClientPromise: Promise<MongoClient> | undefined;
 }
 
-if (/*process.env.NODE_ENV === "development" */ true) {
-  // Em desenvolvimento, usamos uma variável global para preservar a conexão
-  // durante o recarregamento de módulo causado por HMR (Hot Module Replacement)
-  if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options);
-    global._mongoClientPromise = client.connect().catch((err) => {
-      console.error("Erro ao conectar ao MongoDB:", err);
-      throw err;
-    });
-  }
-  clientPromise = global._mongoClientPromise;
+if (!global._mongoClientPromise) {
+  client = new MongoClient(uri, options);
+  global._mongoClientPromise = client.connect();
 }
-//  else {
-//   // Em produção, é melhor não usar uma variável global
-//   client = new MongoClient(uri, options);
-//   clientPromise = client.connect().catch((err) => {
-//     console.error("Erro ao conectar ao MongoDB:", err);
-//     throw err;
-//   });
-// }
 
-// Adicione um tratamento para verificar a conexão
+clientPromise = global._mongoClientPromise;
+
 clientPromise
   .then(() => {
     console.log("MongoDB client conectado com sucesso");
@@ -45,6 +30,4 @@ clientPromise
     console.error("Falha ao conectar o cliente MongoDB:", err);
   });
 
-// Exporta uma promessa do cliente MongoClient. Ao fazer isso em um
-// módulo separado, o cliente pode ser compartilhado entre funções.
 export default clientPromise;
